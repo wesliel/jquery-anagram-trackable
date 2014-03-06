796494e2db4e6915bf8827dc61e1f9c91f94ca55
Weslie Leung <wleung@madebyuppercut.com>
2014-03-06 11:20:48 -0700

    Implement event tracking

--- spec/javascripts/trackableSpec.js
+++ spec/javascripts/trackableSpec.js
@@ -1,47 +1,51 @@
 
 describe("A document", function() {
-	var $fixtures, $fixtureForm, valObj, testOptions;
+  var $fixtures, $fixtureForm, valObj, testOptions;
 
-	beforeEach(function() {
-		loadFixtures('trackable.html');
-		$fixtureForm = $('#fixture');
+  beforeEach(function() {
+    loadFixtures('trackable.html');
+    $('#fixture').anagramTrackable();
 
-		// Mock Google Analystics object to spy on
-		ga = function() {};
-		spyOn(window, 'ga');
-	});
+    // Mock Google Analystics object to spy on
+    ga = function() {};
+    spyOn(window, 'ga');
+  });
 
-	it("should track mouseover event, with category", function() {
-		$('#track-mouseover').trigger('mouseover');
+  it("should track mouseover event, with category", function() {
+    $('#track-mouseover').trigger('mouseover');
+    expect(window.ga).toHaveBeenCalledWith('send', 'event', 'category1', 'mouseover');
+  });
 
-		expect(window.ga).toHaveBeenCalledWith();
-	});
 
+  it("should track mouseout and click event, with category, label, and numeric value", function() {
+    $('#track-mouseout-click').trigger('mouseout');
+    expect(window.ga).toHaveBeenCalledWith('send', 'event', 'category2', 'mouseout', 'label1');
 
-	it("should track mouseout and click event, with category, label, and numeric value", function() {
-		$('#track-mouseout').trigger('mouseout');
-		
-		expect(window.ga).toHaveBeenCalledWith();
+    window.ga.calls.reset();
 
-		$('#track-mouseout').trigger('click');
-		expect(window.ga).toHaveBeenCalledWith();
-	});
+    $('#track-mouseout-click').trigger('click');
+    expect(window.ga).toHaveBeenCalledWith('send', 'event', 'category2', 'click', 'label1');
 
-	it("should track click event, with category, nonInteraction, and no value", function() {
-		$('#track-click-no-hit-no-value').trigger('click');
-		
-		expect(window.ga).toHaveBeenCalledWith();
-	});
+  });
 
-	it("should track click event, with category and a page-hit", function() {
-		$('#track-click-hit').trigger('click');
-		
-		expect(window.ga).toHaveBeenCalledWith();
-	});
+  it("should track click event, with category, nonInteraction, and no value", function() {
+    $('#track-click-no-hit-no-value').trigger('click');
+    expect(window.ga).toHaveBeenCalledWith('send', 'event', 'category3', 'click', { nonInteraction : 1 });
+  });
 
-	it("should track JavaScript error thrown", function() {
-		var x = 9, sum = x + y;
+  it("should track click event, with category and a page-hit", function() {
+    $('#track-click-hit').trigger('click');
+    expect(window.ga).toHaveBeenCalledWith('send', 'event', 'category4', 'click', { page : '/my-page' });
+  });
 
-		expect(window.ga).toHaveBeenCalledWith();
-	});
+  it("should track JavaScript error thrown", function() {
+    // This try/catch block is to work-around native JavaScript error stopping Jasmine from continuing
+    try {
+      var x = 9, sum = x + y;
+    } catch(err) {
+      window.onerror(err.message, window.location, 44);
+    }
+
+    expect(window.ga).toHaveBeenCalledWith('send', 'exception', { exDescription : 'http://localhost:9090/SpecRunner.html?catch=false:44:y is not defined' });
+  });
 });
--- src/jquery.anagram-trackable.0.1.js
+++ src/jquery.anagram-trackable.0.1.js
@@ -9,78 +9,101 @@
  *
  * Date: 2014-03-06T08:29:27Z
  */
+
+oldErrorHandler =  window.onerror;
+
 (function($) {
-	$.fn.anagramTrackable = function(options) {
-		/************************************************************************************
-		 * Plugin defaults and globals
-		 ***********************************************************************************/
-		var
-			GA_SEND = 'send',
-			GA_EVENT = 'event',
-			GA_EXCEPTION = 'exception',
-			GA_EXCEPTION_DESCRIPTION = 'exDescription',
-			GA_NO_HIT = 'nonInteraction',
-			GA_PAGE = 'page',
-			oldErrorHandler = window.onerror;
+  $.fn.anagramTrackable = function(options) {
+    /************************************************************************************
+     * Plugin defaults and globals
+     ***********************************************************************************/
+    var
+      DEFAULTS = {},
+      GA_SEND = 'send',
+      GA_EVENT = 'event',
+      GA_EXCEPTION = 'exception',
+      GA_EXCEPTION_DESCRIPTION = 'exDescription',
+      GA_NO_HIT = 'nonInteraction',
+      GA_PAGE = 'page';
 
-		/**
-		 * Global error handler to send JavaScript error logs to Google Analytics
-		 * @param {string} message The error message
-		 * @param {string} url The source URL that threw the exception
-		 * @param {number} line The line number
-		 * @return {boolean} Always returns false to propagate to other error handlers
-		 */
-		window.onerror = function(message, url, line) {
-			var gaData = {};
+    /**
+     * Global error handler to send JavaScript error logs to Google Analytics
+     * @param {string} message The error message
+     * @param {string} url The source URL that threw the exception
+     * @param {number} line The line number
+     * @return {boolean} Always returns false to propagate to other error handlers
+     */
+    window.onerror = function(message, url, line) {
+      var gaData = {};
 
-			if (typeof ga !== 'undefined') {
-				gaData[GA_EXCEPTION_DESCRIPTION] = url + ':' + line + ':' + message;
-				ga('send', GA_EXCEPTION, gaData);
-			}
+      if (typeof ga !== 'undefined') {
+        gaData[GA_EXCEPTION_DESCRIPTION] = url + ':' + line + ':' + message;
+        ga('send', GA_EXCEPTION, gaData);
+      }
 
-			oldErrorHandler.apply(window, arguments);
-			return false;
-		};
+      if (oldErrorHandler) {
+        oldErrorHandler.apply(window, arguments);
+      }
 
-		/************************************************************************************
-		 * Class definition
-		 ***********************************************************************************/
+      return false;
+    };
 
-		/**
-		 * Class trackable object.
-		 * @param {object} $context The jQuery DOM object.
-		 * @param {object} options The options which would over-ride the default settings.
-		 */
-		function AnagramTrackable($context, options) {
+    /************************************************************************************
+     * Class definition
+     ***********************************************************************************/
 
-			/************************************************************************************
-			 * Private methods
-			 ***********************************************************************************/
+    /**
+     * Class trackable object.
+     * @param {object} $context The jQuery DOM object.
+     * @param {object} options The options which would over-ride the default settings.
+     */
+    function AnagramTrackable($context, options) {
 
-			/**
-			 * Inspect an element's data attributes and resolve the event it need.
-			 * @param {string} str The master string containing possible placeholder strings.
-			 * @param {object} mapObj Mapping of searching strings as keys, with a replacement string as its value.
-			 * @return {string} This returns the replaced string.
-			 */
-			function _resolveCustomEvent() {
-			}
+      /************************************************************************************
+       * Event handler methods
+       ***********************************************************************************/
 
-			/***********************************************************************************/
+      /**
+       * Event handler for trigger a GA event
+       * @param {event} event The jQuery event object triggered.
+       */
+      function _gaEventHandler(event) {
+        var
+          $this = $(this),
+          gaData = {},
+          gaArguments = [];
 
-			var _settings = $.extend(true, DEFAULTS, options);
+        if ($this.is('[data-no-hit]')) { gaData[GA_NO_HIT] = 1; }
+        if ($this.is('[data-hit]')) { gaData[GA_PAGE] = $this.data('hit'); }
 
-			$context.find('[data-trackable]').each(_resolveCustomEvent);
+        // Required arguments
+        gaArguments.push('send', 'event', $this.data('category'), event.type);
 
-			return this;
-		}
+        if ($this.data('label')) { gaArguments.push($this.data('label')); }
+        if ($this.data('value') && !isNaN(parseInt($this.data('value'), 10))) { gaArguments.push(parseInt($this.data('value'), 10)); }
+        if (Object.keys(gaData).length > 0) { gaArguments.push(gaData); }
 
-		// Ensuring if context is a collection of forms, each one is initialized with its own AnagramTrackable instance.
-		// Returning the jQuery context for further chaining.
-		return this.each(function() {
-			if (!$(this).data('anagram-trackable-object')) {
-				$(this).data('anagram-trackable-object', new AnagramTrackable($(this), options));
-			}
-		});
-	};
+        ga.apply(window, gaArguments);
+      }
+
+      /***********************************************************************************/
+
+      var _settings = $.extend(true, DEFAULTS, options);
+
+      $context.find('[data-trackable]').each(function() {
+        var $this = $(this);
+        $this.on($this.data('event'), _gaEventHandler);
+      });
+
+      return this;
+    }
+
+    // Ensuring if context is a collection of forms, each one is initialized with its own AnagramTrackable instance.
+    // Returning the jQuery context for further chaining.
+    return this.each(function() {
+      if (!$(this).data('anagram-trackable-object')) {
+        $(this).data('anagram-trackable-object', new AnagramTrackable($(this), options));
+      }
+    });
+  };
 }(jQuery));
